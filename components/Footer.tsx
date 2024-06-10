a270440b10b2d2f6e4ddbb562d65f69b827f7685
kokorolx <kokoro.lehoang@gmail.com>
2024-06-10 23:59:39 +0700

    update build

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import siteMetadata from 'content/siteMetadata';
 import Link from 'next/link';
 import { Suspense } from 'react';
 import { AiFillLinkedin } from 'react-icons/ai';
-import { FaGithub, FaSpotify, FaSteam, FaTwitter } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 import SectionContainer from './SectionContainer';
 import NowPlaying from './Spotify/NowPlaying';
 
@@ -36,31 +36,6 @@ export default function Footer() {
               <li>
                 <a href={siteMetadata.github} target="_blank" rel="noreferrer" aria-label="github">
                   <FaGithub className="sm:text-lg" />
-                </a>
-              </li>
-              <li>
-                <a
-                  href={siteMetadata.twitter}
-                  target="_blank"
-                  rel="noreferrer"
-                  aria-label="twitter"
-                >
-                  <FaTwitter className="sm:text-lg" />
-                </a>
-              </li>
-              <li>
-                <a
-                  href={siteMetadata.spotify}
-                  target="_blank"
-                  rel="noreferrer"
-                  aria-label="spotify"
-                >
-                  <FaSpotify className="sm:text-lg" />
-                </a>
-              </li>
-              <li>
-                <a href={siteMetadata.steam} target="_blank" rel="noreferrer" aria-label="steam">
-                  <FaSteam className="sm:text-lg" />
                 </a>
               </li>
             </ul>
