b5d10df6ef337b1360a8b34cd9eb8cd8f036ec2c
kokorolx <kokoro.lehoang@gmail.com>
2024-06-11 13:37:03 +0700

    update logrocket

--- components/LogRocket.tsx
+++ components/LogRocket.tsx
@@ -3,7 +3,7 @@
 import rocket from 'logrocket';
 
 export default function LogRocket() {
-  const logrocketId = process.env.NEXT_PUBLIC_LOGROCKET_ID || '';
+  const logrocketId = process.env.NEXT_PUBLIC_LOGROCKET_ID || 'cgz5qz/thnkandgrowcom';
 
   setTimeout(() => {
     rocket.init(logrocketId);
