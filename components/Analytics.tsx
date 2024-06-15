ba2ec2590891d63ae20dc558caaa9163450b0639
kokorolx <kokoro.lehoang@gmail.com>
2024-06-15 23:27:34 +0700

    fix: add GA

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -3,6 +3,7 @@
 // import LogRocket from '@/components/LogRocket';
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function Analytics() {
   return (
@@ -10,6 +11,7 @@ export default function Analytics() {
       {/* <LogRocket /> */}
       <VercelAnalytics />
       <SpeedInsights />
+      <GoogleAnalytics gaId="G-KD7JRK2YJ6" />
     </>
   );
 }
--- content/headerNavLinks.ts
+++ content/headerNavLinks.ts
@@ -1,8 +1,9 @@
 const headerNavLinks = [
   { href: 'https://blog.thnkandgrow.com', title: 'Blog' },
   // { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
   { href: '/uses', title: 'Uses' },
+  { href: '/about', title: 'About' },
+  // { href: '/books', title: 'Books' },
 ];
 
 export default headerNavLinks;
